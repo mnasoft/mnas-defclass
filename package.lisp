d563ec654ec4e8dcf3bc62c0931d142723ae8232
Mykola Matvyeyev <mnasoft@gmail.com>
2023-03-12 20:05:19 +0200

    Sun 12 Mar 2023 06:05:19 PM UTC

--- package.lisp
+++ package.lisp
@@ -1,5 +1,6 @@
 ;;;; package.lisp
 
-(defpackage :mnas-defclass (:use #:cl)  )
+(defpackage :mnas-defclass
+  (:use #:cl))
 
-;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
+(in-package :mnas-defclass)
